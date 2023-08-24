6ee69e04217681c744bad32828cb52bb52718743
Javier Cabrera <xppcoder@gmail.com>
2023-08-24 18:06:19 +0200

    WIP

--- experiments/jobs/dynamic_diversity/oracle_diff_trace_mem.sh
+++ experiments/jobs/dynamic_diversity/oracle_diff_trace_mem.sh
@@ -1,6 +1,8 @@
 trace_file=$1
 wasm=$2
 
+echo "$trace_file $wasm" > trace.txt
+
 /host_based/tracer/pintool/pin/pin -t /host_based/tracer/pintool/obj-intel64/tracer.so -i 0 -m 1 -M 0 -c 0 -b 0 -o orig.ins.txt -- tracer $wasm
 
 # check if the trace.ins.txt is the same as the trace_file, return 1 if they are the same
