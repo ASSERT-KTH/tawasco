a1b199891e822a37e5edec56d91140a33c4a93f4
Javier Cabrera <xppcoder@gmail.com>
2023-08-25 10:56:21 +0200

    FIX: oracles

--- experiments/jobs/dynamic_diversity/oracle_diff_trace_ins.sh
+++ experiments/jobs/dynamic_diversity/oracle_diff_trace_ins.sh
@@ -4,15 +4,19 @@ wasm=$2
 
 /host_based/tracer/pintool/pin/pin -t /host_based/tracer/pintool/obj-intel64/tracer.so -i 1 -m 0 -c 1 -b 1  -o trace.ins.txt -- tracer $wasm
 
+
 md5sum trace.ins.txt
 md5sum "$trace_file"
 
+hash1=$(sha256sum trace.ins.txt | cut -d ' ' -f 1)
+hash2=$(sha256sum "$trace_file" | cut -d ' ' -f 1)
 
 # check if the trace.ins.txt is the same as the trace_file, return 1 if they are the same
 # check the md5sum of the trace.ins.txt and trace_file
 # check if the md5sum is the same
-if cmp -s "trace.ins.txt" "$trace_file"; then
-    echo "Same trace"
+if [ "$hash1" != "$hash2" ]
+then
+    echo "Dif trace"
     exit 1
 else
     exit 0
--- experiments/jobs/dynamic_diversity/oracle_diff_trace_mem.sh
+++ experiments/jobs/dynamic_diversity/oracle_diff_trace_mem.sh
@@ -7,13 +7,20 @@ wasm=$2
 md5sum trace.mem.txt
 md5sum "$trace_file"
 
+hash1=$(sha256sum trace.mem.txt | cut -d ' ' -f 1)
+hash2=$(sha256sum "$trace_file" | cut -d ' ' -f 1)
+
 # check if the trace.ins.txt is the same as the trace_file, return 1 if they are the same
-if cmp -s "trace.mem.txt" "$trace_file"; then
-    echo "Same trace"
+# check the md5sum of the trace.ins.txt and trace_file
+# check if the md5sum is the same
+if [ "$hash1" != "$hash2" ]
+then
+    echo "Diff trace"
     exit 1
 else
     exit 0
 fi
+
 
 
 
